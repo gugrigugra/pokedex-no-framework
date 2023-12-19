f4ef3e92b63fef89f8b11bf3c10874042b9d88af
Gabriele Di Cosmo <gabrieledicosmo05@gmail.com>
2023-12-19 18:00:14 +0100

    fine dei lavori

---
+++ static/js/index.js
@@ -0,0 +1,85 @@
+const pokemonName = document.querySelector(".pokemon_name");
+const pokemonNumber = document.querySelector(".pokemon_number");
+const pokemonImage = document.querySelector(".pokemon_image");
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input_search");
+const buttonPrev = document.querySelector(".btn-prev");
+const buttonNext = document.querySelector(".btn-next");
+
+let searchPokemon;
+
+const fetchPokemon = async (pokemon) => {
+	const APIResponse = await fetch(
+		`https://pokeapi.co/api/v2/pokemon/${pokemon}`
+	);
+
+	if (APIResponse.status === 200) {
+		const data = await APIResponse.json();
+		return data;
+	}
+};
+
+const renderPokemon = async (pokemon) => {
+	pokemonName.innerHTML = "Loading...";
+	pokemonNumber.innerHTML = "";
+
+	const data = await fetchPokemon(pokemon);
+
+
+
+	if (data) {
+		console.log(data);
+		pokemonImage.style.display = "block";
+		pokemonName.innerHTML = data.name;
+		pokemonNumber.innerHTML = data.id;
+		pokemonImage.src =
+			data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+				"front_default"
+			];
+		input.value = "";
+		searchPokemon = data.id;
+
+        
+		statsBox.innerHTML = `
+        <h2>${data.name} Stats</h2>
+        <p>Height: ${data.height}</p>
+        <p>Weight: ${data.weight}</p>
+        <p>HP: ${data.stats[0].base_stat}</p>
+        <p>Attack: ${data.stats[1].base_stat}</p>
+        <p>Defense: ${data.stats[2].base_stat}</p>
+    `;
+
+        if (data.types.length === 1) {
+            statsBox.innerHTML += `<p>Type: ${data.types[0].type.name}</p>`;
+            
+        } else {
+            statsBox.innerHTML += `<p>Type: ${data.types[0].type.name}, ${data.types[1].type.name}</p>`;
+        }
+
+		statsBox.classList.remove("hidden");
+	} else {
+		pokemonImage.style.display = "none";
+		pokemonName.innerHTML = "Who's that Pokemon?";
+		pokemonNumber.innerHTML = "";
+	}
+};
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	renderPokemon(input.value.toLowerCase());
+});
+
+buttonPrev.addEventListener("click", () => {
+	if (searchPokemon > 1) {
+		searchPokemon -= 1;
+		renderPokemon(searchPokemon);
+	}
+});
+
+buttonNext.addEventListener("click", () => {
+	searchPokemon += 1;
+	renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
